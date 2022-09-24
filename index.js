f9af484bdeb493220c03384a601dec32439c3d77
vanbsanne <93342385+vanbsanne@users.noreply.github.com>
2022-09-24 15:13:05 +0200

    added disablebutton

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var positive1 = document.querySelector('#input1');
 var positive2 = document.querySelector('#input2');
 var positive3 = document.querySelector('#input3');
@@ -5,6 +6,8 @@ var positive3 = document.querySelector('#input3');
 var saveButton = document.querySelector('#saveButton');
 var forwardButton = document.querySelector('#forwardButton');
 var backButton = document.querySelector('#backButton');
+
+const days = "days";
 
 function save() {
     // Get positives fromtext inputs
@@ -20,22 +23,45 @@ function save() {
         pos3: pos3,
     };
 
-    // Ensure list of previous days exists
-    if (localStorage.getItem('days') == undefined) {
-        let emptyList = [];
-        let emptyJson = JSON.stringify(emptyList);
-        localStorage.setItem('days', emptyJson);
-    }
+    listEmpty();
 
     // Get previous days
-    let days = JSON.parse(localStorage.getItem('days'));
+    let days = JSON.parse(localStorage.getItem(days));
 
     // Add new day to list of days
     days.push(day);
 
     // Save new list of days
     let json = JSON.stringify(days);
-    localStorage.setItem('days', json);
+    localStorage.setItem(days, json);
 }
+
+function checkDate() { //check if date is filled 
+    listEmpty();
+    let json = localStorage.getItem(days); //get json 
+    let list = JSON.parse(json); //make it a list again
+    let today = new Date().toLocaleString('nl-BE').split(' ')[0];
+
+    for(let i=0;i<days.length;i++){
+        if(days[i].date == today){
+            return true;
+        }
+    } return false;
+}
+
+function listEmpty(){ //Ensure list of previous days exists
+    if (localStorage.getItem(days) == undefined) {
+        let emptyList = [];
+        let emptyJson = JSON.stringify(emptyList);
+        localStorage.setItem(days, emptyJson);
+    }
+}
+
+function buttonEnabled(){
+    if (checkDate()==true){
+        document.getElementById("saveButton").disabled = true;
+    }
+}
+
 
 saveButton.addEventListener('click', save);
